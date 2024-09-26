ff00a2807c64c5e152195ebf7f3504df5fe7cf10
diem <levietha170205@gmail.com>
2024-09-26 11:34:10 +0700

    practice

--- code.js
+++ code.js
@@ -1,1 +1,8 @@
-console.log('hello')
+//console.log('hello')
+let name ="Ha";
+let age = 20;
+let isStudent= true;
+ console.log("Name : " +name);
+ console.log("Age: "+age);
+ console.log("la hoc sinh "+ isStudent);
+ 
