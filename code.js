a7a7c6e3f841d7fb736e8d2f4533e076466739e7
diem <levietha170205@gmail.com>
2024-09-27 13:55:10 +0700

    practices

--- code.js
+++ code.js
@@ -1,18 +1,19 @@
 console.log('hello')
+//khai bao bien
 let name ="Ha";
 let age = 20;
 let isStudent= true;
  console.log("Name : " +name);
  console.log("Age: "+age);
  console.log("la hoc sinh "+ isStudent);
-
+//ham tong
 function sum(a, b){
     s=a+b;
     return s;
  }
 console.log(sum(5.1, 6));
 console.log(sum(7, 12));
-
+// kiem tra so chan và so le
 function checkOddEven(number){
     if(number%2==0)
         console.log(number +" la so chan.");
@@ -20,5 +21,10 @@ function checkOddEven(number){
 }
 checkOddEven(9);
 checkOddEven(18);
+// vong lap
+for(let i=0;i<=10;i++){
+    console.log(i);
+}
+
 
 
