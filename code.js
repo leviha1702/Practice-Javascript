e6f442d101b93ffad2ccd6f96f94a714d0384ff9
diem <levietha170205@gmail.com>
2024-09-27 13:44:30 +0700

    sum and odd,even

--- code.js
+++ code.js
@@ -1,8 +1,24 @@
-//console.log('hello')
+console.log('hello')
 let name ="Ha";
 let age = 20;
 let isStudent= true;
  console.log("Name : " +name);
  console.log("Age: "+age);
  console.log("la hoc sinh "+ isStudent);
- 
+
+function sum(a, b){
+    s=a+b;
+    return s;
+ }
+console.log(sum(5.1, 6));
+console.log(sum(7, 12));
+
+function checkOddEven(number){
+    if(number%2==0)
+        console.log(number +" la so chan.");
+    else console.log(number +" la so le.");
+}
+checkOddEven(9);
+checkOddEven(18);
+
+
