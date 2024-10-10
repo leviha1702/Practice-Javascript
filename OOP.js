c814eacad783c061c828d65bd81c4c62bde01cbb
diem <levietha170205@gmail.com>
2024-10-10 10:33:13 +0700

    Inheritance

--- OOP.js
+++ OOP.js
@@ -1,3 +1,4 @@
+//OPP
 class laptop{
     constructor(brand,model,year){
         this.brand = brand;
@@ -14,3 +15,15 @@ class laptop{
 const myLaptop = new laptop("Asus","Vivobook 15",2024);
 myLaptop.buy ();
 console.log(myLaptop.sell());
+//Inheritance:tính kế thừa
+class phone extends laptop{
+    constructor(brand,model,year,phoneNumber){
+    super(brand,model,year);//goi lai construct để thừa hưởng
+    this.phoneNumber=phoneNumber;
+    }
+    own(){
+        console.log(`I'm owning ${this.brand} ${this.model} ${this.year} and mt phone number:${this.phoneNumber}`);
+    }
+}
+const myPhone = new phone("Iphone","11 Pro Max",2024,"00039828");
+myPhone.own();
